0c9b8e70291cb02f31166517dafc582a1494dd68
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2017-11-16 17:07:12 -0800

    Add routes for arena and heroes components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { ArenaComponent } from './arena/arena.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: DashboardComponent
+	},
+	{
+		path: 'heroes',
+		component: HeroesComponent
+	},
+	{
+		path: 'arena',
+		component: ArenaComponent
 	}
 ];
 
