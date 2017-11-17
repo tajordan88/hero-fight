e9dfb8932cda10504900e97135569610261b0229
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2017-11-16 16:52:04 -0800

    Add dashboard to router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: DashboardComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
