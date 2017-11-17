422ff8e779a7a603bf87286f3f19e1d710eee9c8
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2017-11-16 16:52:45 -0800

    Create heroes component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
