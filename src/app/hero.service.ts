c0ec4cc3fff955399dccb1a53c176d58e42b1204
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2017-11-17 17:24:00 -0800

    Add service for heroes with Observable.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 @Injectable()
 export class HeroService {
+
+	getHeroes(): Observable<Hero[]> {
+		return of(HEROES);
+	}
 
   constructor() { }
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+	heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+  	this.getHeroes();
+  }
+
+  getHeroes(): void {
+  	this.heroService.getHeroes()
+  			.subscribe(heroes => this.heroes = heroes);
   }
 
 }
